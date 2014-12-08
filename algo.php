48345d10a5d293d4454a3caaeea825d6aee6f9d1
i.vlasenko <you@example.com>
2014-12-08 23:57:52 +0200

    javascript part

--- algo.php
+++ algo.php
@@ -9,12 +9,8 @@ $d->setLogger($logger);
 $left_bank = new Pool();
 $left_bank->push(new Father('Papa', 1))
             ->push(new Mother('Mom', 2))
-            ->push(new Mother('Mom2', 5))
             ->push(new Child('daughter Anna', 3))
             ->push(new Child('son Rob', 4))
-            ->push(new Child('son Rob1', 6))
-            ->push(new Child('son Rob2', 7))
-            ->push(new Child('son Rob3', 8))
             ->push(new Fisher('Fisher', 10));
 
 $d->setLeftBank($left_bank);
